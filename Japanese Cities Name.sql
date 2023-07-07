628bb0c22aeabdf2dcf3c13c6399d14113e5a929
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-07 17:46:08 +0530

    Japanese Cities Name.sql

---
+++ Japanese Cities Name.sql
@@ -0,0 +1,10 @@
+/*
+
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+The CITY table is described as follows:
+
+*/
+
+//Code
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN';
