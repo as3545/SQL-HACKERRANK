f56a678460423c0cdb39dc6742ffc0541b705d8e
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-07 17:53:01 +0530

    Weather Observation Station 3.sql

---
+++ Weather Observattion Station 3.sql
@@ -0,0 +1,8 @@
+/*
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+
+*/
+
+//Code:
+
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
