e360ad3028778fdc044d584de9c618a9128c778b
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-07 18:21:35 +0530

    Weather Observation Station 10.sql

---
+++ Weather Observation Station 10.sql
@@ -0,0 +1,9 @@
+/*
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+*/
+//Code
+
+SELECT DISTINCT(CITY)
+  FROM STATION
+ WHERE CITY REGEXP '[^aeiouAEIOU]$';
