4fb97deb34f427585065de84ebfe748a9cf157d8
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-07 17:56:40 +0530

    Weather Observation Station 6.sql

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,13 @@
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+*/
+
+
+//Code
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY LIKE 'A%'
+OR CITY LIKE 'E%'
+OR CITY LIKE 'I%'
+OR CITY LIKE 'O%'
+OR CITY LIKE 'U%';
