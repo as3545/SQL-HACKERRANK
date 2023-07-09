3452d0fd39b3e04a585cbe8ed5d9f799d5f693f1
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-09 11:39:35 +0530

    Weather Observation station 2.sql

--- Weather Observation station 2.sql
+++ Weather Observation station 2.sql
@@ -1,8 +1,8 @@
 /*
 Query the following two values from the STATION table:
 
-The sum of all values in LAT_N rounded to a scale of  decimal places.
-The sum of all values in LONG_W rounded to a scale of  decimal places.
+The sum of all values in LAT_N is rounded to a scale of  decimal places.
+The sum of all values in LONG_W is rounded to a scale of  decimal places.
 Input Format
 
 The STATION table is described as follows:
