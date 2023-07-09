d68447bb2f17b92bb164191910613fb74b36e7e3
as3545 <86479682+as3545@users.noreply.github.com>
2023-07-09 12:09:17 +0530

    Weather Observation Station 13.sql

--- Weather Observation Station 13.sql
+++ Weather Observation Station 13.sql
@@ -1,5 +1,5 @@
 /*
-Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to  decimal places.
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345 . Truncate your answer to 4 decimal places.
 */
 
 //Code
