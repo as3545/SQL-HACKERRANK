37eca76ddcf14fef1b049340719ed751d9f0ab7e
as3545 <86479682+as3545@users.noreply.github.com>
2024-02-06 19:59:46 +0530

    Weather observation Station 9.sql

--- Weather observation Station 9.sql
+++ Weather observation Station 9.sql
@@ -2,5 +2,8 @@
 Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
 
 */
-//Code
-SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[aeiuo]';
+//CodeSELECT DISTINCT CITY WHERE CITY NOT LIKE 'A%'
+AND CITY NOT LIKE 'E%'
+AND CITY NOT LIKE 'I%'
+AND CITY NOT LIKE 'O%'
+AND CITY NOT LIKE 'U%';
