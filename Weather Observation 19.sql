87e0fcdc671c801130dcd3f58c38de6cca75c49b
as3545 <86479682+as3545@users.noreply.github.com>
2024-02-16 13:31:20 +0530

    Weather Observation 19.sql

---
+++ Weather Observation 19.sql
@@ -0,0 +1,15 @@
+/*
+Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+
+Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+SELECT ROUND(SQRT(POW(MAX(LAT_N) - MIN(LAT_N), 2) + POW(MAX(LONG_W) - MIN(LONG_W), 2)), 4) AS Euclidean_Distance
+FROM STATION;
